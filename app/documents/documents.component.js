eea75e05c1b2a386734ef1a1a87509eacfda3437
Anthony <AntCon1000@gmail.com>
2020-01-16 15:11:54 -0500

    Added proposals templates

--- app/documents/documents.component.js
+++ app/documents/documents.component.js
@@ -17,21 +17,21 @@ var DocumentsComponent = (function () {
                 title: "My First Doc",
                 description: 'asdfasdfasdf asdfasd',
                 file_url: 'http://google.com',
-                updated_at: '01/15/2020',
+                updated_at: '11/11/16',
                 image_url: 'http://google.com',
             },
             {
                 title: "My Second Doc",
                 description: 'asdfasdfasdf asdfasd',
                 file_url: 'http://google.com',
-                updated_at: '01/15/2020',
+                updated_at: '11/11/16',
                 image_url: 'http://google.com',
             },
             {
                 title: "My Last Doc",
                 description: 'asdfasdfasdf asdfasd',
                 file_url: 'http://google.com',
-                updated_at: '01/15/2020',
+                updated_at: '11/11/16',
                 image_url: 'http://google.com',
             }
         ];
